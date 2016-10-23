0edeaf607078b8f05e2df3f72ed49c1473d27b98
Enoch <enoch@craftgag.com>
2016-10-24 00:26:08 +0530

    CraftGag Inc.
    Made in MVC.

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Site
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-2.1.4.min.js",
+                        "~/Scripts/isotope.min.js",
+                        "~/Scripts/ytplayer.min.js",
+                        "~/Scripts/owl.carousel.min.js",
+                        "~/Scripts/lightbox.min.js",
+                        "~/Scripts/twitterfetcher.min.js",
+                        "~/Scripts/smooth-scroll.min.js",
+                         "~/Scripts/scrollreveal.min.js",
+                        "~/Scripts/parallax.js",
+                        "~/Scripts/scripts.js" ));
+
+           bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
+                      "~/Content/socioicon.css",
+                      "~/Content/iconsmind.css",
+                      "~/Content/owl.carousel.css",
+                      "~/Content/lightbox.min.css",
+                      "~/Content/theme.css").Include("~/Content/interface-icons.css", new CssRewriteUrlTransform()));
+           
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           return View();
+        }
+
+       
+        public ActionResult services()
+        {
+           return View();
+        }
+
+        public ActionResult overview()
+        {
+            return View();
+        }
+
+        public ActionResult portfolio()
+        {
+           return View();
+        }
+
+        public ActionResult contact()
+        {
+           return View();
+        }
+       
+    }
+}
